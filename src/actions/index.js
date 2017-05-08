81f0062ce7f7730445449eb566b30b15f797e508
njdaniel <nicholasjdaniel@gmail.com>
2017-05-08 17:19:43 -0400

    ajax call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 /**
  * Created by nicholas on 5/3/17.
  */
+import axios from 'axios';
+
+export function fetchdata() {
+    const request = axios.get('https://crest-tq.eveonline.com/market/10000002/orders/sell/?type=https://crest-tq.eveonline.com/inventory/types/34/');
+}
